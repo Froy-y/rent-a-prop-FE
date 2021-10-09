4eb4947cef802f037728e049caa09cbf8b7561ae
Froylan <froy.push@gmail.com>
2021-10-08 21:02:55 -0500

    Functional create component !

--- src/components/NewRental.js
+++ src/components/NewRental.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const NewRental = (props) => {
+    const initialState = {
+        name: '',
+        address: ''
+    }
+    const [input, setInput] = useState(initialState)
+
+    //fetch for post
+    const newRental = async (data) => {
+        try {
+            const configs = {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+
+            const createdRental = await fetch('http://localhost:9000/renta', configs)
+            const parsed = await createdRental.json()
+            props.history.push('/renta')
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const handleChange = (e) => {
+        setInput({...input, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setInput(initialState)
+        newRental(input)
+    }
+
     return(
         <>
-            <h1>Create a Rental!</h1>
+            <div>
+                <form onSubmit={ handleSubmit }>
+                    <label htmlFor="name">Name</label>
+                    <input id="name" name="name" value={ input.name } onChange={handleChange}/>
+                    
+                    <label htmlFor="address">address</label>
+                    <input id="address" name="address" value={ input.address } onChange={handleChange}/>
+
+                    <input type="submit" value="Add a property" />
+                </form>
+                <Link to="/renta">Back</Link>
+            </div>
         </>
     )
 }
