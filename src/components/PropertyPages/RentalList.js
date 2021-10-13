7caed0ed94705a23cfa44251dfc0e2c63d95b6d8
Froylan <froy.push@gmail.com>
2021-10-13 16:56:38 -0500

    Added a card and offcanvas

--- src/components/PropertyPages/RentalList.js
+++ src/components/PropertyPages/RentalList.js
@@ -1,53 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { useParams } from "react-router-dom"
-import { getUserToken } from '../../utils/authToken'
 import Card from 'react-bootstrap/Card'
 import { useHistory } from "react-router-dom"
 import Button from "react-bootstrap/Button"
 import Offcanvas from "react-bootstrap/Offcanvas"
 
-const RentalList = (props) => {
+const RentalList = props => {
     const [rental, setRental] = useState([])
     const history = useHistory()
     const [show, setShow] = useState(false)
-    const { userId } = useParams()
-
+    
     //fetch for index
     const getRentals = async () => {
         try {
-            console.log(getUserToken())
-            const configs = {
-                method: "GET",
-                body: JSON.stringify(),
-                headers: {
-                  "Content-Type": "application/json",
-                  "Authorization": `bearer ${getUserToken()}`,
-                  }
-              }
-            const allRentals = await fetch(`http://localhost:9000/${userId}/renta`, configs)
-            console.log(allRentals)
+            const allRentals = await fetch('http://localhost:9000/renta')
             const parsed = await allRentals.json()
-            console.log(parsed)
             setRental(parsed)
         } catch (err) {
             console.log(err)
         }
     }
+    useEffect(() => {getRentals()}, [])
 
-    useEffect(() => {
-        getRentals()
-    }, [])
-
-    const handleDelete = async (rentaId) => {
+    const handleDelete = async id => {
         try {
             const config = {
-                method: 'DELETE',
-                headers: {
-                    "Authorization": `bearer ${getUserToken()}`
-                }
+                method: 'DELETE'
             }
-            const deleteRental = await fetch(`http://localhost:9000/${userId}/renta/${rentaId}`, config)
+            const deleteRental = await fetch(`http://localhost:9000/renta/${id}`, config)
             const parsed = await deleteRental.json()
             const updateRental = rental.filter(rentalProperty => rentalProperty._id !== parsed._id)
             setRental(updateRental)
@@ -63,6 +43,24 @@ const RentalList = (props) => {
     return(
         <>
             <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Address</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { rental && rental.map(rentalProperty => (
+                            <tr key={ rentalProperty._id }>
+                                <td>{ rentalProperty.name }</td>
+                                <td><Link to={`/renta/${rentalProperty._id}`}>{ rentalProperty.address }</Link></td>
+                                <td onClick={() => handleDelete(rentalProperty._id)}>X</td>
+                                
+                            </tr>
+                        )) }
+                    </tbody>
+                </table>
                 { rental && rental.map(property => (
                 <div className="cardDiv">
                     <Card style={{ width: '18rem' }} onClick={ handleShow } className="cursorCard">
@@ -76,7 +74,7 @@ const RentalList = (props) => {
                             <Button 
                             variant="primary"
                             size="lg"
-                            onClick={() => handleClick(`/${userId}/renta/${property._id}`)}
+                            onClick={() => handleClick(`/renta/${property._id}`)}
                             >
                             View
                             </Button>
@@ -92,9 +90,9 @@ const RentalList = (props) => {
                     </Offcanvas>
                 </div>
                 ))}
-                <Link to={`/${userId}/renta/new`}>Create New Rental</Link>
+                <Link to="/renta/new">Create New Rental</Link>
                 <br/>
-                <Link to={`/${userId}/home`}>Back</Link>
+                <Link to="/">Dismiss</Link>
             </div>
         </>
     )
